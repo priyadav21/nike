e55e8f0473e1c5b8d199bd1e589f81182cdbcbe0
Priya Yadav <priyadav218@gmail.com>
2024-01-08 21:12:00 +0530

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import PopularProductCard from "./PopularProductCard";
+import ShoeCard from "./ShoeCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+
+export { Button, Nav, PopularProductCard, ShoeCard, ServiceCard, ReviewCard };
